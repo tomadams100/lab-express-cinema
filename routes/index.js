8a020a57a2d0bdbe9a15d801e3cf23ce03c357fb
Tom Adams <thomasjeadams@btinternet.com>
2021-11-14 22:23:09 +0100

    finished lab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req,res) => {
+    try {
+        const allMovies = await Movie.find()
+        res.render('movies', {allMovies})
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+router.get('/movie/:id', async (req,res) => {
+    try {
+        const {id} = req.params
+        const movie = await Movie.findById(id)
+        res.render("movieDetails", {movie})
+    } catch (err) {
+        console.log("ERROR>>>>>> ",err)
+    }
+})
 
 module.exports = router;
